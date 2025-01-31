509ee84c071c92a8791164b9bb7535a178b2b73d
Kanchan1030 <168181660+Kanchan1030@users.noreply.github.com>
2025-01-31 15:57:09 +0530

    Create Solution08.R

---
+++ Unit01_Assignment/Solution08.R
@@ -0,0 +1,8 @@
+seq_part <- 3:6
+repeat_part <- rep(c(2, -5.1, -33), 2)
+value_part <- 7/42 + 2
+my_vector <- c(seq_part, repeat_part, value_part)
+my_vector
+
+omit_first_last <- my_vector[-c(1, length(my_vector))]
+omit_first_last
